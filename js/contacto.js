dad271382858ff79ea10125506de8b91498fe56d
Franjazani <85326083+Franjazani@users.noreply.github.com>
2023-10-09 23:44:00 -0300

    subi contacto js

--- js/contacto.js
+++ js/contacto.js
@@ -1,22 +1,85 @@
 const formulario = document.getElementById("formulario");
-const userName = document.getElementById('userNames');
-const userEmail = document.getElementById('userEmail');
+const userName = document.getElementById("userName");
+const userEmail = document.getElementById("userEmail");
+const celular = document.getElementById("celular");
 
-const regUserName = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-const regUserEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+const alertSuccess = document.getElementById("alertSuccess");
+const alertName = document.getElementById("alertName");
+const alertEmail = document.getElementById("alertEmail");
+const alertCelular = document.getElementById("alertCelular");
 
+
+    const regUserName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+    const regUserEmail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
+    const regCelular = /^\d+$/gi;
+
+const pintarMensajeExito = () => {
+    alertSuccess.classList.remove("d-none");
+    alertSuccess.textContent = "Mensaje enviado con éxito";
+};
+
+const pintarMensajeError = (errores) => {
+    errores.forEach((item) => {
+        item.tipo.classList.remove("d-none");
+        item.tipo.textContent = item.msg;
+    });
+};
 
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    if (!regUserName.test(userNames.value)) {
-        console.log('Formato no valido');
-        return;
+
+    alertSuccess.classList.add("d-none");
+    const errores = [];
+
+    // validar nombre
+    if (!regUserName.test(userName.value) || !userName.value.trim()) {
+        userName.classList.add("is-invalid");
+
+        errores.push({
+            tipo: alertName,
+            msg: "Formato no válido campo nombre, solo letras",
+        });
+    } else {
+        userName.classList.remove("is-invalid");
+        userName.classList.add("is-valid");
+        alertName.classList.add("d-none");
     }
-    if (!regUserEmail.test(userEmail.value)) {
-        console.log('Formato no valido');
+
+    // validar email
+    if (!regUserEmail.test(userEmail.value) || !userEmail.value.trim()) {
+        userEmail.classList.add("is-invalid");
+
+        errores.push({
+            tipo: alertEmail,
+            msg: "Escriba un correo válido",
+        });
+    } else {
+        userEmail.classList.remove("is-invalid");
+        userEmail.classList.add("is-valid");
+        alertEmail.classList.add("d-none");
+    }
+
+    if (errores.length !== 0) {
+        pintarMensajeError(errores);
         return;
     }
 
-    console.log('formulario enviado');
-})
+    // validar Celular
+    
+    if (!regCelular.test(celular.value) || !celular.value.trim()) {
+        celular.classList.add("is-invalid");
+
+        errores.push({
+            tipo: alertCelular,
+            msg: "Formato no válido campo Telefono, solo numeros",
+        });
+    } else {
+        celular.classList.remove("is-invalid");
+        celular.classList.add("is-valid");
+        alertCelular.classList.add("d-none");
+    }
+
+    console.log("Formulario enviado con éxito");
+    pintarMensajeExito();
+});

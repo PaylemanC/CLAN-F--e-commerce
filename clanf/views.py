f576083662e376b4f37677ef6d73e651924b705e
Carina Payleman <rociocarina2@gmail.com>
2023-12-03 19:10:49 -0300

    Reemplaza title CLAN-F dinámico

--- clanf/views.py
+++ clanf/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, 'index.html', {
+        'title': 'CLAN-F'
+    })
 
 
 def quienes_somos(request):

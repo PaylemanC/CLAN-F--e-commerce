c573aa1bb175399c062e91bd187bd8a2cb3a1808
Franjazani <85326083+Franjazani@users.noreply.github.com>
2023-12-05 00:38:58 -0300

    form sin renderizar en el modal

---
+++ productos/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Product
+
+class ProductForm(forms.ModelForms):
+    class Meta: 
+        model = Product
+        fields = ['title', 'price', 'stock']
+    
+    title = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Nombre del producto'
+        })
+    )
--- productos/views.py
+++ productos/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import ProductForm
 
 # Create your views here.
+def crear_producto(request):
+    form = ProductForm(request.POST or None)
+    if request.method == 'POST': 
+        form = ProductForm(request.POST) #para Yeimer, crea una instancia de productForm
+        if form.is_valid():
+            form.save()
+            return redirect('administrador.html')
+    else: #si el metodo es GET genera una instancia y se la pasa al contexto
+        form = ProductForm()
+    return render(request, 'administrador.html', {
+        'form': form
+    })

3c821bfa718d72ba4ff6238d4e537829b41f6970
Carina Payleman <rociocarina2@gmail.com>
2023-12-05 16:12:08 -0300

    Mueve función de crear_producto a administrador y soluciona bug form

--- clanf/views.py
+++ clanf/views.py
@@ -1,6 +1,7 @@
 # views.py ==> Tener todas la funciones o clases para renderizar las vistas de una aplicación, es decir, mostrar los HTML.
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from productos.forms import ProductForm
 
 def index(request):
     return render(request, 'index.html', {
@@ -17,4 +18,14 @@ def lista_productos(request):
     return render(request, 'productos.html', {})
 
 def administrador(request):
-    return render(request, 'administrador.html', {})
+    form = ProductForm(request.POST or None)
+    if request.method == 'POST': 
+        form = ProductForm(request.POST) #para Yeimer, crea una instancia de productForm
+        if form.is_valid():
+            form.save()
+            return redirect('administrador.html')
+    else: #si el metodo es GET genera una instancia y se la pasa al contexto
+        form = ProductForm()
+    return render(request, 'administrador.html', {
+        'form': form
+    })
--- productos/forms.py
+++ productos/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Product
 
-class ProductForm(forms.ModelForms):
+class ProductForm(forms.ModelForm):
     class Meta: 
         model = Product
         fields = ['title', 'price', 'stock']

7247e413dde8d6d1bd68c73e20640d392ccea0d6
Franjazani <85326083+Franjazani@users.noreply.github.com>
2023-12-05 21:20:14 -0300

    bootstrap form

--- productos/forms.py
+++ productos/forms.py
@@ -6,8 +6,21 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ['title', 'price', 'stock']
     
-    title = forms.CharField(
+    title = forms.CharField(label='Producto', min_length=1, max_length=30,
         widget=forms.TextInput(attrs={
-            'placeholder': 'Nombre del producto'
+            'placeholder': 'Nombre del Producto',
+            'class': "form-control",
+        })
+    )
+    price = forms.CharField(label='Precio', min_length=1, max_length=8,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Indique un Precio',
+            'class': "form-control",
+        })
+    )
+    stock = forms.CharField(label='Stock', min_length=0, max_length=5,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Indique la Cantidad',
+            'class': "form-control",
         })
     )

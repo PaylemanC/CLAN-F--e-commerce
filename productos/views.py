292bc2619dabb4c2c177a3c4c6a0dec8d8bcb4ea
Yeimer Alberto <mr.yeimeralberto@gmail.com>
2023-12-07 22:28:15 -0300

    Interpolación en product-admin

--- productos/views.py
+++ productos/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import ProductForm
 from django.contrib.auth.decorators import login_required
+from productos.models import Product
 
 # Create your views here.
+@login_required(login_url='/login')
+def listar_productos(request):
+    products = Product.objects.all()
+    print(products)
+    return render(request, 'administrador.html', {
+        'products': products
+    })
+
 @login_required(login_url='/login')
 def crear_producto(request):
     form = ProductForm(request.POST or None)
     if request.method == 'POST': 
-        form = ProductForm(request.POST) #para Yeimer, crea una instancia de productForm
+        form = ProductForm(request.POST) #Crea una instancia de productForm
         if form.is_valid():
             form.save()
             return redirect('administrador.html')
